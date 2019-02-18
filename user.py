8f905e9c9acc647d32a99c42d63594f6260b14d1
JackGlaister <jackglaister@gmail.com>
2019-02-18 20:22:55 +0000

    file moved

---
+++ user.py
@@ -0,0 +1,21 @@
+class User:
+    self.__ID = none
+    self.__name = none
+    self.__password = none
+    self.__essionToken = none
+    def verifyLogin():
+        #This is a routine which returns a bool value of true or false for the user's login
+        #If true is returned, this means taht user will be able to login and will be greeted with an appropriate welcome screen. 
+        #Otherwise the user will be told their login information is wrong and will be sent back to the login screen to try again. 
+
+class Student(user):
+    def __init__():
+        #this is where the student will be instantiated. It will be assigned a token here based on it's username and the date (an hour potentially) after it logs in
+
+class Lecturer(user):
+    def __setFormativeTest():
+    def __setSummativeTest():
+    def __setDeadline():
+    def __init__():
+    	#this is where the lecturer class will be instantiated for the given Lecturer information. 
+
