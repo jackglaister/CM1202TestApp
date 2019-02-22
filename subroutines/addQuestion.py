db3ac6088fc4a8ab57f0159eea9ff0c60cad077a
JackGlaister <jackglaister@gmail.com>
2019-02-22 17:31:33 +0000

    with errors

---
+++ subroutines/addQuestion.py
@@ -0,0 +1,23 @@
+#/bin/python3
+def addQuestion(testID, questionID, questionType, questionText, answer, choices):
+    """
+    >>> addQuestion("Q0001",bool(1),"How many unique representations can 8 bits make","256","none")
+    >>> import csv
+    >>> f = open("000115.csv","r")
+    >>> fr = csv.reader(f)
+    >>> LastItem = []
+    >>> for row in fr:
+    ...     LastItem = row
+    >>> print(LastItem)
+    >>> print(LastItem[0] == "Q0001")
+    True
+    >>> print(LastItem[1] == "1")
+    True
+    >>> print(LastItem[2] == "How many unique representations can 8 bits make")
+    True
+    >>> print(LastItem[3] == "256")
+    True
+    """
+    f = open(str(testID)+".csv","a")
+    f.write(str(questionID)+","+str(int(questionType))+","+str(questionText)+","+str(answer)+","+str("choices"))
+    f.close()
