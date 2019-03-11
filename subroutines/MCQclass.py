1ff91950f021ea1ebe441fa5e984052420e26c34
ssaif1 <48092438+ssaif1@users.noreply.github.com>
2019-03-11 14:23:45 +0000

    Add files via upload

---
+++ subroutines/MCQclass.py
@@ -0,0 +1,28 @@
+class addMultipleChoiceQuestion:
+	def addQuestion(testID, questionID, questionType, questionText, answer, choices): 
+
+     """ 
+     >>> addMultipleChoiceQuestion("000120","Q0001",bool(1),"In what year was python first released?","1990","1970","2002","1985","1990") 
+     >>> import csv 
+     >>> f = open("000115.csv","r") 
+     >>> fr = csv.reader(f) 
+     >>> LastItem = [] 
+     >>> for row in fr: 
+     ...     LastItem = row 
+     >>> print(LastItem[0] == "Q0001") 
+     True 
+     >>> print(LastItem[1] == "1") 
+     True 
+     >>> print(LastItem[2] == "In what year was python first released?") 
+     True 
+     >>> print(LastItem[3] == "1990") 
+     True 
+     >>> print(LastItem[4] == "["1970","2002","1985","1990"])
+    
+     """ 
+     f = open(str(testID)+".csv","a") 
+     f.write(str(questionID)+","+str(int(questionType))+","+str(questionText)+","+str(answer)+","+str(choices)+"\n") 
+     f.close() 
+
+
+
