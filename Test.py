63ecfcbe46921d1768859f05a6847766020e9976
jackglaister <44065198+jackglaister@users.noreply.github.com>
2019-03-31 06:48:42 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -26,6 +26,38 @@ class Test:
             self.__questions = question()
 
 class multipleChoiceQuestion(Question):
+    def addQuestion(testID, questionID, questionType, questionText, answer, choices): 
+    """ 
+    >>> addMultipleChoiceQuestion("000120","Q0001",bool(1),"In what year was python first released?","1990","1970","2002","1985","1990") 
+    >>> import csv 
+    >>> f = open("000115.csv","r") 
+    >>> fr = csv.reader(f)
+    >>> LastItem = [] 
+    >>> for row in fr: 
+    ...     LastItem = row 
+    >>> print(LastItem[0] == "Q0001") 
+    True 
+    >>> print(LastItem[1] == "1") 
+    True 
+    >>> print(LastItem[2] == "In what year was python first released?") 
+    True 
+    >>> print(LastItem[3] == "1990") 
+    True 
+    >>> print(LastItem[4] == "["1970","2002","1985","1990"])
+   
+    """ 
+    f = open(str(testID)+".csv","a") 
+    f.write(str(questionID)+","+str(int(questionType))+","+str(questionText)+","+str(answer)+","+str(choices)+"\n") 
+    f.close() 
+    def __init__(self, text, answer, questionNumber, choices)
+        self._text = text
+        self._answer = answer
+        self.questionNumber = questionNumber
+        self._choices = choices
+    def editQuestion(self, text, answer, choices):
+        self._text = text
+        self._answer = answer
+        self._choices = choices
     
 
 class Question:
@@ -33,4 +65,3 @@ class Question:
         self._text = text
         self._answer = answer 
         self.questionNumber = questionNumber
-
