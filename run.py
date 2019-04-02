04ec140cb9da1ac0195a006f17a761e3f2e22717
Jack Glaister <glaisterj@cardiff.ac.uk>
2019-04-02 04:54:05 +0100

    run updated

--- run.py
+++ run.py
@@ -1,8 +1,17 @@
+import csv
 from Test import *
 from user import *
 
 def userLogin():
     return User()
+
+def importTestsList():
+    file = open("Tests.csv")
+    csvf = csv.reader(file)
+    a = []
+    for line in csvf:
+        a.append(line)
+    return a
 
 if __name__ == '__main__':
     user = False
